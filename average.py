99a5fb89273200a537685fb36d655c412ea4c1fa
SayatSaguin <aeronsayat24@gmail.com>
2021-09-22 05:06:59 +0000

    1st Commit

---
+++ average.py
@@ -0,0 +1,10 @@
+print("Your Average Grade")
+pre=int(input("Enter Prelim:  "))
+mid=int(input("Enter Midterm:  "))
+sem=int(input("Enter SemiFinal:  "))
+fin=int(input("Enter Final:  "))
+
+tots= pre+mid+sem+fin
+ave=tots/4
+
+print("The Average is ",ave)
